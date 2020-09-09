52f3fec19981fd2dc5ceb503ff30afd14b77a9d8
James Rowe <jamesrowe@outlook.com>
2020-09-09 14:27:31 -0400

    currently broken

--- client/src/pages/UploadFile.js
+++ client/src/pages/UploadFile.js
@@ -22,7 +22,7 @@ export default class UploadFile extends Component {
         e.preventDefault()
         const formData = new FormData()
         formData.append('profileImg', this.state.profileImg)
-        axios.post("http://localhost:4000/api/user-profile", formData, {
+        axios.post("http://localhost:8000/api/user-profile", formData, {
         }).then(res => {
             console.log(res)
         })
