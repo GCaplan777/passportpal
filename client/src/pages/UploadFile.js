cc74f509e9449fb9320d74b124f47d28f274c82b
James Rowe <jamesrowe@outlook.com>
2020-09-09 13:43:37 -0400

    grid fs stuff

---
+++ client/src/pages/UploadFile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+var fs = require('fs');
+var mongoose = require("mongoose");
+var Grid = require('gridfs-stream');
+var GridFS = Grid(mongoose.connection.db, mongoose.mongo);
+
+function putFile(path, name, callback) {
+    var writestream = GridFS.createWriteStream({
+        filename: name
+    });
+    writestream.on('close', function (file) {
+      callback(null, file);
+    });
+    fs.createReadStream(path).pipe(writestream);
+}
+
+const Home = () => {
+  return <div>Hello world</div>;
+};
+
+export default Home;
+
