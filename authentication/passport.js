69ba22f65c050f0216b9d7a5678ba7df9bb19ae5
Henry Liu <hl3184@columbia.edu>
2020-09-09 14:46:36 -0400

    login and register setup

--- authentication/passport.js
+++ authentication/passport.js
@@ -1,25 +1,25 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const passportJWT = require("passport-jwt");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
-const db = require("../models");
+const db = require('../models');
 
 // local strategy
 passport.use(
   new LocalStrategy(
     {
-      usernameField: "email",
-      passwordField: "password",
+      usernameField: 'email',
+      passwordField: 'password',
     },
     function (email, password, cb) {
       //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
       return db.User.findOne({ email, password })
         .then((user) => {
           if (!user) {
-            return cb(null, false, { message: "Incorrect email or password." });
+            return cb(null, false, { message: 'Incorrect email or password.' });
           }
-          return cb(null, user, { message: "Logged In Successfully" });
+          return cb(null, user, { message: 'Logged In Successfully' });
         })
         .catch((err) => cb(err));
     }
@@ -32,7 +32,7 @@ passport.use(
     {
       // {Authorization : "Bearer (the code in 3 parts separated by ."}
       jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: "your_jwt_secret",
+      secretOrKey: 'secret',
     },
     function (jwtPayload, cb) {
       //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
