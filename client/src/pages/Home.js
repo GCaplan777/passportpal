ce35a12c2478f520c1bcc42522946656cf5d709d
GCaplan <gabriel.caplan@gmail.com>
2020-09-11 11:51:21 -0400

    initial Hello World utilizing material ui button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { useGlobalContext } from "./context/GlobalContext";
-import AuthenticatedApp from "../components/AuthenticatedApp";
-import UnauthenticatedApp from "../components/AuthenticatedApp";
+// import { useGlobalContext } from "./GlobalContext.js";
+import Button from "@material-ui/core/Button";
 
 const Home = () => {
-  const [state, dispatch] = useGlobalContext();
-  return <div>Hello world</div>;
-  {
-    state.user.token ? <AuthenticatedApp /> : <UnauthenticatedApp />;
-  }
+  // const [state, dispatch] = useGlobalContext();
+  return (
+    <Button variant="contained" color="primary">
+      Hello world
+    </Button>
+  );
 };
 
 export default Home;
