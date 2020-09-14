75ec2fc74790060a6475a200b8c27d449f42cb4d
Henry Liu <hl3184@columbia.edu>
2020-09-14 17:36:07 -0400

    update

--- models/user.js
+++ models/user.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  profileImg: {
-    type: String,
-  },
-});
+    _id: mongoose.Schema.Types.ObjectId,
+    profileImg: {
+        type: String
+    }
+}, {
+    collection: 'users'
+})
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
