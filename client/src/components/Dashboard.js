a713b0b3cf0e81122c3a774c213554165b6bcac8
Henry Liu <hl3184@columbia.edu>
2020-09-15 11:18:31 -0400

    style with MUI

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import DashboardList from './Constants';
 import DashCard from './DashCard';
 
-import './dashBoard.css';
-
 export default function Dashboard() {
+  const useStyles = makeStyles((theme) => ({
+    cardContainer: {
+      display: 'grid',
+      'grid-template-columns': 'repeat(auto-fit, minmax(250px, 1fr))',
+      'grid-gap': '20px',
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div className="dashBoard">
+    <div className={classes.cardContainer}>
       {DashboardList.map((dashCardObj) => (
         <DashCard key={dashCardObj.id} {...dashCardObj} />
       ))}
