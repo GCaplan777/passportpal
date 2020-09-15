e10b5a9502c92f4e266e90ee3012d66a9c8b8826
Henry Liu <hl3184@columbia.edu>
2020-09-14 22:00:30 -0400

    chatting page completed

--- client/src/components/StickyFooter.js
+++ client/src/components/StickyFooter.js
@@ -1,27 +1,27 @@
-import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary">
-      {"Copyright © "}
+      {'Copyright © '}
       <Link color="inherit" href="https://github.com/GCaplan777/passportpal">
         Passport Pal
-      </Link>{" "}
+      </Link>{' '}
       {new Date().getFullYear()}
-      {"."}
+      {'.'}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    flexDirection: "column",
+    display: 'flex',
+    flexDirection: 'column',
     // minHeight: "100vh",
   },
   main: {
@@ -32,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3, 2),
     // marginTop: "auto",
     backgroundColor:
-      theme.palette.type === "light"
+      theme.palette.type === 'light'
         ? theme.palette.grey[200]
         : theme.palette.grey[800],
   },
@@ -51,9 +51,7 @@ export default function StickyFooter() {
       ></Container>
       <footer className={classes.footer}>
         <Container maxWidth="sm">
-          <Typography variant="body1">
-            {/* My sticky footer can be found here. */}
-          </Typography>
+          <Typography variant="body1"></Typography>
           <Copyright />
         </Container>
       </footer>
