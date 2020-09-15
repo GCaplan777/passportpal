e69ec437d2e93b3ee1429349bd0a4127044e9443
James Rowe <jamesrowe@outlook.com>
2020-09-14 23:40:06 -0400

    user.js

--- models/user.js
+++ models/user.js
@@ -5,8 +5,8 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  documentsUploaded: {
-    type: Array,
+  planeTicket: {
+    type: String,
   },
 });
 
